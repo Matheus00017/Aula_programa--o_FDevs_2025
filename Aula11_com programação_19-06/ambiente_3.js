8eec14de8fe836420999226a685dba273091e032
Matheus00017 <vidalmatheus321@gmail.com>
2025-06-19 19:18:15 -0300

    Aula_19-06

---
+++ Aula11_com programação_19-06/ambiente_3.js
@@ -0,0 +1,16 @@
+let num = [5, 4, 6, 7, 9 ,8 ,1 ,2 ,3];
+
+/*
+num[10] = 10;
+num[9] = 11;
+num[10] = 200
+num.push(12);
+num.length
+*/
+
+num.sort();
+
+
+console.log(num)
+console.log(`nosso vetor é o ${num} e tem ${num.length} posições`);
+console.log(`o primeiro valor do vetor é o número ${num[0]}`);
