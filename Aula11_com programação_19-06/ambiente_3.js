85594f383c092ed79cced13c73d0c33f7edc5452
Matheus00017 <vidalmatheus321@gmail.com>
2025-06-23 16:23:40 -0300

    Aula_23-06_

--- Aula11_com programação_19-06/ambiente_3.js
+++ Aula11_com programação_19-06/ambiente_3.js
@@ -1,4 +1,4 @@
-let num = [5, 4, 6, 7, 9 ,8 ,1 ,2 ,3];
+let num = [5, 6, 7, 9 ,8 ,1 ,2 ,3];
 
 /*
 num[10] = 10;
@@ -9,8 +9,12 @@ num.length
 */
 
 num.sort();
-
-
 console.log(num)
 console.log(`nosso vetor é o ${num} e tem ${num.length} posições`);
 console.log(`o primeiro valor do vetor é o número ${num[0]}`);
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log(`O valor não foi encontrado`);
+}else{
+    console.log(`O valor está na posição ${pos}`)
+}
