3598f6c437f54274b0126e3fef740f0744d22996
Matheus00017 <vidalmatheus321@gmail.com>
2025-11-29 17:52:14 -0300

    Aula_28-11

--- Angular/curso-angular/src/app/app.ts
+++ Angular/curso-angular/src/app/app.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { HttpClientModule} from '@angular/common/http'
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HttpClientModule],
+  imports: [RouterOutlet, RouterLink],
   template: `
     <h1>Meu Site Angular</h1>
 
     <a routerLink="/">Home</a> |
     <a routerLink="/list">Lista</a> |
-    <a routerLink="/parent">parent</a> |
-    <a routerLink="/item">item</a>
+    <a routerLink="/parent">parent</a>
 
     <router-outlet></router-outlet>
   `
